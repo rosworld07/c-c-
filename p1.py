2b13618d4e59669c235845a70113f4184106b2af
rosworld07 <43461446+rosworld07@users.noreply.github.com>
2020-11-12 16:43:56 +0530

    wap to display your name on the screen

---
+++ p1.py
@@ -0,0 +1,19 @@
+
+
+
+def isEven(x):   
+     if x%2==0:   
+         return x  
+     else:  
+         return False
+        
+l=[0,5,10,15,20,25,30]   
+l1=list(filter(isEven,l))   
+print(l1)  
+
+
+
+    
+    
+
+
